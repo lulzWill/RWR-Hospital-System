88c82ca48fbceb68971452cd9e181d481c029014
Ross Kistinger <rkistinger12@gmail.com>
2015-03-07 18:38:14 -0600

    added signup page

---
+++ signupuser.php
@@ -0,0 +1,19 @@
+<?php 
+	require 'vendor/autoload.php';
+	use Parse\ParseClient;
+	use Parse\ParseUser;
+	use Parse\ParseException;
+	
+	ParseClient::initialize('kHbyXSdw4DIXw4Q0DYDcdM8QTDQnOewKJhc9ppAr', '9h80LHVDFOSAgVQ1NSPf5IgaWAaDnHdPoJWt2CDc', '3q1HVOiiywyBdtalMN1sozceJbNXuD9WKZSSmgvI');
+	
+	try {
+	  $user = ParseUser::logIn($_POST["username"], $_POST["password"]);
+	  // Do stuff after successful login.
+	  header("Location: signupsuccess.html");
+	  exit;
+	} catch (ParseException $error) {
+	  // The login failed. Check error to see why.
+	  header("Location: test.html");
+	  exit;
+	}
+?>
