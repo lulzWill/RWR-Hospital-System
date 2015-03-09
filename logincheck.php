7e18ce0a5dc849d0587e7e2c50ef352fc9e4096f
lulzWill <william-pruyn@uiowa.edu>
2015-03-09 17:13:20 -0500

    added email confirmation
    added email validation to the signup page as well as a check for
    validated email in the login logic

--- logincheck.php
+++ logincheck.php
@@ -3,11 +3,21 @@
 	use Parse\ParseClient;
 	use Parse\ParseUser;
 	use Parse\ParseException;
+	use Parse\ParseQuery;
 	
 	ParseClient::initialize('kHbyXSdw4DIXw4Q0DYDcdM8QTDQnOewKJhc9ppAr', '9h80LHVDFOSAgVQ1NSPf5IgaWAaDnHdPoJWt2CDc', '3q1HVOiiywyBdtalMN1sozceJbNXuD9WKZSSmgvI');
 	
-
+	$query = ParseUser::query();
+	$query->equalTo("username", $_POST["username"]);
+	$results = $query->find();
+	$userValidate = $results[0];
+	$userValidate = $userValidate->get("emailVerified");
 	
+	if(!$userValidate)
+	{
+		header("Location: emailerr.html");
+		exit;
+	}
 	try {
 	  $user = ParseUser::logIn($_POST["username"], $_POST["password"]);
 	  // Do stuff after successful login.
---
+++ resendconfirmation.php
@@ -0,0 +1,9 @@
+<?php
+	require 'vendor/autoload.php';
+	use Parse\ParseClient;
+	use Parse\ParseUser;
+	use Parse\ParseException;
+	use Parse\ParseQuery;
+	
+	ParseClient::initialize('kHbyXSdw4DIXw4Q0DYDcdM8QTDQnOewKJhc9ppAr', '9h80LHVDFOSAgVQ1NSPf5IgaWAaDnHdPoJWt2CDc', '3q1HVOiiywyBdtalMN1sozceJbNXuD9WKZSSmgvI');
+?>
