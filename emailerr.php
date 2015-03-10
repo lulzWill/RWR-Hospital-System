46018c319542d861311f07c5be4dc1db70fdce19
lulzWill <william-pruyn@uiowa.edu>
2015-03-09 23:07:42 -0500

    nag and status message changes
    swapped the verification error page over to .php and added some helpful
    links to the navbar

--- emailerr.php
+++ emailerr.php
@@ -22,6 +22,8 @@
 	      <script src="https://oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
 	      <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
 	    <![endif]-->
+			
+	  <?php include_once("navbar.html"); ?>
 	  </head>
 	  <body>
 		<body>
@@ -33,10 +35,10 @@
 				</p>
 				<div class="btn-group btn-group-justified" role="group" aria-label="...">
 				  <div class="btn-group" role="group">
-				    <a href="test.html" class="btn btn-default">Log In</a>
+				    <a href="test.php" class="btn btn-default">Log In</a>
 				  </div>
 				  	<div class="btn-group" role="group">
-				    	<a href="signup.html" class="btn btn-default">Sign Up</a>
+				    	<a href="signup.php" class="btn btn-default">Sign Up</a>
 				  	</div>
 				  
 				  	<div class="btn-group" role="group">
--- loginsuccess.php
+++ loginsuccess.php
@@ -1,34 +1,53 @@
-<!DOCTYPE html>
-<html lang="en">
-  <head>
-  	<title>
-  		Hospital Login Page
-  	</title>
-    <meta charset="utf-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
+<HTML>
+<HEAD>
+	<Title>Error: Must Validate Email</Title>
+</HEAD>
+<BODY>
+	<!DOCTYPE html>
+	<html lang="en">
+	  <head>
+	  	<title>
+	  		Hospital Login Page
+	  	</title>
+	    <meta charset="utf-8">
+	    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+	    <meta name="viewport" content="width=device-width, initial-scale=1">
 
-    <!-- Bootstrap -->
-    <link href="css/bootstrap.min.css" rel="stylesheet">
-	<link href="customcss.css" rel="stylesheet">
-    <!-- HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries -->
-    <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
-    <!--[if lt IE 9]>
-      <script src="https://oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
-      <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
-    <![endif]-->
-    <?php include_once("navbar.html"); ?>
-  </head>
-  <body>
-	<body>
-		<h1>
-			Login Successful
-		</h1>
-	</body>
+	    <!-- Bootstrap -->
+	    <link href="css/bootstrap.min.css" rel="stylesheet">
+		<link href="errMess.css" rel="stylesheet">
+	    <!-- HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries -->
+	    <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
+	    <!--[if lt IE 9]>
+	      <script src="https://oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
+	      <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
+	    <![endif]-->
+			
+	  <?php include_once("navbar.html"); ?>
+	  </head>
+	  <body>
+		<body>
+			<div class="opbox h1">
+				<p>
+				</br>
+				</br>          Login Successful!
+				</p>
+				<div class="btn-group btn-group-justified" role="group" aria-label="...">
+				  <div class="btn-group" role="group">
+				    <a href="test.php" class="btn btn-default">Log In</a>
+				  </div>
+				  	<div class="btn-group" role="group">
+				    	<a href="signup.php" class="btn btn-default">Sign Up</a>
+				  	</div>
+				</div>
+			</div>
+		</body>
 
-    <!-- jQuery (necessary for Bootstrap's JavaScript plugins) -->
-    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.2/jquery.min.js"></script>
-    <!-- Include all compiled plugins (below), or include individual files as needed -->
-    <script src="js/bootstrap.min.js"></script>
-  </body>
-</html>
+	    <!-- jQuery (necessary for Bootstrap's JavaScript plugins) -->
+	    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.2/jquery.min.js"></script>
+	    <!-- Include all compiled plugins (below), or include individual files as needed -->
+	    <script src="js/bootstrap.min.js"></script>
+	  </body>
+	</html>
+</BODY>
+</HTML>
