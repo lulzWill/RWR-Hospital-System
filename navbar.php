9b5b0c6fbd5e3d249f39840320a4679c1e703742
lulzWill <william-pruyn@uiowa.edu>
2015-03-27 12:47:06 -0500

    navbar updates

--- navbar.php
+++ navbar.php
@@ -18,18 +18,6 @@
       <ul class="nav navbar-nav">
         <li class="active"><a href="https://github.com/lulzWill/RWR-Hospital-System">GitHub <span class="sr-only">(current)</span></a></li>
         <li><a href="https://parse.com/apps/hospital-management-system--2/collections#class/_User">Parse DB</a></li>
-        <li class="dropdown">
-          <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-expanded="false">Dropdown <span class="caret"></span></a>
-          <ul class="dropdown-menu" role="menu">
-            <li><a href="#">Action</a></li>
-            <li><a href="#">Another action</a></li>
-            <li><a href="#">Something else here</a></li>
-            <li class="divider"></li>
-            <li><a href="#">Separated link</a></li>
-            <li class="divider"></li>
-            <li><a href="#">One more separated link</a></li>
-          </ul>
-        </li>
       </ul>
       <ul class="nav navbar-nav navbar-right">
 <?php
@@ -68,16 +56,6 @@ EOL;
 EOL;
 	}
 	?>
-        <li class="dropdown">
-          <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-expanded="false">Dropdown <span class="caret"></span></a>
-          <ul class="dropdown-menu" role="menu">
-            <li><a href="#">Action</a></li>
-            <li><a href="#">Another action</a></li>
-            <li><a href="#">Something else here</a></li>
-            <li class="divider"></li>
-            <li><a href="#">Separated link</a></li>
-          </ul>
-        </li>
       </ul>
     </div><!-- /.navbar-collapse -->
   </div><!-- /.container-fluid -->
